efab7d6c8c0ad76abfdb25e1a22c557e7fb167ca
Douglas Cidral <doug.coding20@gmail.com>
2024-12-23 10:41:27 +0100

    Player climbing action
    making possible for the player to climb the ladder

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,21 +14,26 @@ public class PlayerMovement : MonoBehaviour
     private Animator animator;
     private CapsuleCollider2D playerCollider;
     private Vector2 moveInput;
+    private float startGravity;
 
     private const string IS_RUNNING = "isRunning";
+    private const string IS_IDLE_CLIMBING = "isIdleClimbing";
+    private const string IS_CLIMBING = "isClimbing";
     private const string GROUND_LAYER = "Ground";
+    private const string CLIMBING_LAYER = "Climbing";
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         playerCollider = GetComponent<CapsuleCollider2D>();
+        startGravity = rb.gravityScale;
     }
 
     void FixedUpdate()
     {
+        Climb();
         Run();
-        RunningAnimation();
     }
 
     void OnMove(InputValue value)
@@ -38,7 +43,7 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask(GROUND_LAYER))) return;
+        if (!IsTouchingTheGround()) return;
 
       if(value.isPressed)
         {
@@ -46,23 +51,73 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void Climb()
+    {
+        if (!IsPossibleToClimb())
+        {
+            SetClimbingAnimations(false, false);
+            rb.gravityScale = startGravity;
+            return;
+        }
+
+        ClimbAnimation();
+        rb.velocity = new Vector2(
+            moveInput.y == 0f ? rb.velocity.x : 0f,
+            moveInput.y == 0f ? 0f : moveInput.y * speed
+);
+        rb.gravityScale = 0f;
+
+    }
+
+    private bool IsPossibleToClimb()
+    {
+        return playerCollider.IsTouchingLayers(LayerMask.GetMask(CLIMBING_LAYER));
+    }
+
+    private void SetClimbingAnimations(bool isIdleClimbing, bool isClimbing)
+    {
+        animator.SetBool(IS_IDLE_CLIMBING, isIdleClimbing);
+        animator.SetBool(IS_CLIMBING, isClimbing);
+    }
+
+    private void ClimbAnimation()
+    {
+        if (HasVerticalSpeed())
+        {
+            SetClimbingAnimations(true, true);
+            return;
+        }
+        SetClimbingAnimations(true, false);
+    }
+
+    private bool HasVerticalSpeed()
+    {
+        return Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;
+    }
+
     private void Run()
     {
+        if (!IsTouchingTheGround()) return;
+
+        RunningAnimation();
         Vector2 velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
         rb.velocity = velocity;
+        ChangePlayerDirection();
+    }
+
+    private bool IsTouchingTheGround()
+    {
+        return playerCollider.IsTouchingLayers(LayerMask.GetMask(GROUND_LAYER));
     }
 
     private void RunningAnimation()
     {
-        if (HasHorizontalSpeed())
-        {
-            SetIsRunning(true);
-            ChangePlayerDirection();
-        }
-        else
-        {
-            SetIsRunning(false);
-        }
+        SetRunningAnimation(HasHorizontalSpeed());
+    }
+
+    private void SetRunningAnimation(bool isRunning)
+    {
+        animator.SetBool(IS_RUNNING, isRunning);
     }
 
     private bool HasHorizontalSpeed()
@@ -73,10 +128,5 @@ public class PlayerMovement : MonoBehaviour
     private void ChangePlayerDirection()
     {
         transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
-    }
-
-    private void SetIsRunning(bool isRunning)
-    {
-        animator.SetBool(IS_RUNNING, isRunning);
     }
 }
